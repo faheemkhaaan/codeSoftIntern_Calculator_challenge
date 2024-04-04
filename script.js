d7b560e8c02cd9262879ab951dd10a2ca891f9f2
faheemkhaaan <faheemkhan.pg@gmail.com>
2024-04-04 16:18:06 -0700

    initail commit

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const buttons = document.querySelector('.buttons')
 const result = document.getElementById('result');
 
 let input = "";
+let calculation = ''
 buttons.addEventListener('click', (e) => {
 
     
@@ -11,14 +12,20 @@ buttons.addEventListener('click', (e) => {
 
     if(target.className != 'buttons'){
 
+       
+
         if (target.className === 'equal') {
-            let operation = eval(input);
-    
-            input = String(operation)
-            result.textContent = operation;
+            if(target.textContent !== "AC" && result.textContent !== ''){
+                console.log(input);
+                calculation = eval(input);
+            }
+            
+            console.log(calculation);
+            input = String(calculation)
+            result.textContent = input;
         } else if (
-            (input.slice(input.length - 1)=='-'||input.slice(input.length-1)==="+" || input.slice(input.length - 1)=== '*') && 
-             (target.textContent == '-' ||target.textContent == '+' || target.textContent === "*" )
+            (input.slice(input.length - 1)=='-'||input.slice(input.length-1)==="+" || input.slice(input.length - 1)=== '*' || input.slice(input.length - 1) === '.') && 
+             (target.textContent == '-' ||target.textContent == '+' || target.textContent === "*" || target.textContent == '.')
             ) {
             console.log('contains -');
             input = result.textContent;
@@ -26,6 +33,11 @@ buttons.addEventListener('click', (e) => {
             input += target.textContent;
         }
         result.textContent = input;
+
+        if(target.textContent == 'AC'){
+            result.textContent = '0'
+            input = '0'
+        }
      
     }
 
